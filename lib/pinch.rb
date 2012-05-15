794ff0d0693746912e98056b409cbd29a4d2147e
Subhash Chandra <subhash@investopresto.com>
2012-05-15 14:56:13 +0530

    forced encoding on unzipped file

--- lib/pinch.rb
+++ lib/pinch.rb
@@ -155,7 +155,7 @@ private
               if length > 0
                 unzipped_chunk = zstream.inflate(chunk.slice(0, length))
                 begin
-                  pipew << unzipped_chunk
+                  pipew << unzipped_chunk.force_encoding("UTF-8")
                 rescue Exception => e
                   puts "error while parsing \"#{unzipped_chunk}\": #{e.class.name} : #{e.message}"
                   puts "#{length} more to be processed"
